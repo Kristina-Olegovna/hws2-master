0a6de314b1cc9ba766eea21345d504b9c8b1f2f1
Kristina-Olegovna <kristiubchik92@gmail.com>
2022-11-29 11:46:13 +0200

    Commit

---
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import s from './FriendMessage.module.css'
+
+type MessageFriendPropsType={
+    message:{
+        id:number,
+        user:{avatar:string, name:string},
+        message:{text:string, time:string}
+    }
+}
+
+// создать тип вместо any и отобразить приходящие данные
+const FriendMessage = (props: MessageFriendPropsType) => {
+    return (
+        <div
+            id={'hw1-friend-message-' + props.message.id}
+            className={s.friendMessage}
+        >
+            <div className={s.friendImageAndText}>
+                <img
+                    id={'hw1-friend-avatar-' + props.message.id}
+                    // создаёт студент
+                    alt={'avatar' + props.message.user.name}
+                    src={props.message.user.avatar}
+                    //
+                />
+                <div className={s.friendText}>
+                    <div
+                        id={'hw1-friend-name-' + props.message.id}
+                        className={s.friendName}
+                    >
+                        {/*создаёт студент*/}
+
+                        {/**/}
+                    </div>
+                    <pre
+                        id={'hw1-friend-text-' + props.message.id}
+                        className={s.friendMessageText}
+                    >
+                        {/*создаёт студент*/}
+                        {props.message.message.text}
+                        {/**/}
+                    </pre>
+                </div>
+            </div>
+            <div
+                id={'hw1-friend-time-' + props.message.id}
+                className={s.friendTime}
+            >
+                {/*создаёт студент*/}
+                {props.message.message.time}
+                {/**/}
+            </div>
+        </div>
+    )
+}
+
+export default FriendMessage
